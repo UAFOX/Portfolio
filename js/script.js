91ddf09ae74c3a38aa2b14d3a74c541ca401ebfa
UAFOX <142724374+UAFOX@users.noreply.github.com>
2023-11-05 15:08:17 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,6 @@
-
+$(document).ready(function() {
+    $('.header__burger').click(function(event) {
+        $('.header__burger,.main-page__header-nav-list,.main-page__header-nav-bottom-container').toggleClass('active');
+        $('body').toggleClass('lock');
+    });
+});
